6d34683895dc6f4d59014b4d62913806f69a4b98
andreyleganov <arickpost@gmail.com>
2020-08-16 18:34:12 +0300

    Add all

---
+++ FTNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FTNews
+//
+//  Created by Andrey Leganov on 16.08.2020.
+//  Copyright © 2020 Леганов Андрей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
